a54d59489164f973d95b4c5ab5d7efab87ff9854
rodrigobustamante <rbustamantejelvez@gmail.com>
2020-03-14 15:19:16 -0300

    Turn off import/prefer-default-export rule in .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,7 @@
 module.exports = {
-  extends: ["./node_modules/poetic/config/eslint/eslint-config.js"],
+  extends: ['./node_modules/poetic/config/eslint/eslint-config.js'],
   // Add custom rules here
-  rules: {}
+  rules: {
+    'import/prefer-default-export': off,
+  },
 };
--- handler.ts
+++ handler.ts
@@ -1,11 +1,9 @@
-"use strict";
-
-import { APIGatewayEvent, APIGatewayProxyResult, Context } from "aws-lambda";
-import server from "./src/index";
+import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
+import server from './src/index';
 
 export const handler = async (
   event: APIGatewayEvent,
-  context: Context
+  context: Context,
 ): Promise<APIGatewayProxyResult> => {
-  return await server(event, context);
+  return server(event, context);
 };
