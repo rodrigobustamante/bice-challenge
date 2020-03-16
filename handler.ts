20f236269790d56d7efe7ccd415d4b36d0225374
rodrigobustamante <rbustamantejelvez@gmail.com>
2020-03-16 16:33:04 -0300

    Remove express and manage the response in the handler

--- handler.ts
+++ handler.ts
@@ -1,7 +1,25 @@
 import { APIGatewayEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
-import server from './src/index';
+import policy from './src/services/policy';
 
 export const handler = async (
-  event: APIGatewayEvent,
-  context: Context,
-): Promise<APIGatewayProxyResult> => server(event, context);
+  _event: APIGatewayEvent,
+  _context: Context,
+): Promise<APIGatewayProxyResult> => {
+  try {
+    const data = await policy();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        ...data,
+      }),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: 'Internal server error',
+      }),
+    };
+  }
+};
